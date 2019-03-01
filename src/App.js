22fba74386bc4dae8aebf2444da79f021373b5e1
Gigi Law <gigi.law.14@gmail.com>
2019-03-01 05:19:55 +0000

    Basic todo app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
+import Todos from './Todos'
 
 class App extends Component {
+	state = {
+		todos: [
+			{ id: 1, content: 'buy milk' },
+			{ id: 2, content: 'play ultimate' },
+		],
+	}
 	render() {
-		return <div className="App" />
+		return (
+			<div className="todo-app container">
+				<h1 className="center blue-text">Today's Todos</h1>
+				<Todos todos={this.state.todos} />
+			</div>
+		)
 	}
 }
 
